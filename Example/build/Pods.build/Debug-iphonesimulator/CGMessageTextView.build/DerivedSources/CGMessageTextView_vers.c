e738607e3abda660355669b6412aea512a7118c3
Gallo, Carmelo <carmelo.gallo@mobgen.com>
2017-06-21 14:51:36 +0200

    [Added] .gitignore file

--- Example/build/Pods.build/Debug-iphonesimulator/CGMessageTextView.build/DerivedSources/CGMessageTextView_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char CGMessageTextViewVersionString[];
- extern const double CGMessageTextViewVersionNumber;
-
- const unsigned char CGMessageTextViewVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:CGMessageTextView  PROJECT:Pods-1" "\n";
- const double CGMessageTextViewVersionNumber __attribute__ ((used)) = (double)1.;
--- Example/build/Pods.build/Release-iphoneos/CGMessageTextView.build/Objects-normal/armv7/CGMessageTextView_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/home/Desktop/Development/Github/CGMessageTextView/Example/build/Pods.build/Release-iphoneos/CGMessageTextView.build/DerivedSources/CGMessageTextView_vers.c
